e4dfd370c3b1faf300b667137544fd37d7b0dd98
Grace <wangxiaotian2012@gmail.com>
2022-06-21 18:58:49 -0700

    Implement POST `/posts/:id/comments` handler

--- comments/index.js
+++ comments/index.js
@@ -6,14 +6,30 @@ const { randomBytes } = require("crypto");
 const app = express();
 app.use(bodyParser.json());
 
-const commentsByPostId = {};
+const comments = {};
 
 app.get("/posts/:id/comments", (req, res) => {
 
 });
 
 app.post("/posts/:id/comments", (req, res) => {
+  // Generate a random id for the comment
+  const commentId = randomBytes(4).toString("hex");
+  const postId = req.params.id;
 
+  const { content } = req.body;
+
+  // The comments for a given post ID is an existing array or undefined
+  // if undefined, initialize to an empty array
+  if (!comments[postId]) {
+    comments[postId] = []
+  };
+
+  // Add the new comment to the array
+  comments[postId].push({ id: commentId, content });
+
+  // Response with the updated comments array for this post
+  res.status(201).send(comments[postId]);
 });
 
 app.listen(4001, () => {
