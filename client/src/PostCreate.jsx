48fa7f44a40bb54f9afa7fee3f3e49289fa9d24f
Grace <wangxiaotian2012@gmail.com>
2022-06-22 15:32:52 -0700

    Make PostCreate input a controlled input

--- client/src/PostCreate.jsx
+++ client/src/PostCreate.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import axios from "axios";
+
 const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
   return (
     <div>
       <form>
         <div className="form-group" >
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            className="form-control"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
         </div>
         <button className="btn btn-primary" >Submit</button>
       </form>
