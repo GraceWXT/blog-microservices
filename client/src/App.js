7d3c403c10f669b5898ec9fbc9cad690c17a65ae
Grace <wangxiaotian2012@gmail.com>
2022-06-22 14:41:28 -0700

    Add the PostCreate Component to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import PostCreate from './PostCreate';
 
 function App() {
   return (
     <div className="App">
       Blog App
+      <h1>Create Post</h1>
+      <PostCreate />
     </div>
   );
 }
