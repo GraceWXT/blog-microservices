b98854b560d28f6a7cc6faa8c2eff7c4df3d6381
Grace <wangxiaotian2012@gmail.com>
2022-06-22 15:45:13 -0700

    Implement the submit logic

--- client/src/PostCreate.jsx
+++ client/src/PostCreate.jsx
@@ -4,9 +4,20 @@ import axios from "axios";
 const PostCreate = () => {
   const [title, setTitle] = useState("");
 
+  const submitForm = async (event) => {
+    event.preventDefault();
+
+    await axios.post("http://localhost:4000/posts", { title });
+
+    setTitle("");
+  };
+
+
   return (
     <div>
-      <form>
+      <form
+        onSubmit={submitForm}
+      >
         <div className="form-group" >
           <label>Title</label>
           <input
