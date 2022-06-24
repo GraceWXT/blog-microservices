23f2102e0364a98fb3de9d02f101bbb00e4997c8
Grace <wangxiaotian2012@gmail.com>
2022-06-23 17:33:06 -0700

    Implement the create comment component

---
+++ client/src/CommentCreate.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import axios from "axios";
+
+const CommentCreate = (props) => {
+  const { postID } = props;
+
+  const [content, setContent] = useState("");
+
+  const submitComment = async (event) => {
+    event.preventDefault();
+    await axios.post(`http://localhost:4001/posts/${postID}/comments`, {content});
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitComment}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+          className="form-control"
+          value={content}
+          onChange={event => setContent(event.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+};
+
+export default CommentCreate;
